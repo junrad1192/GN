33ae23f8ef5ced41db7279a1b376c843d269f777
junrad1192 <junrad1192@gmail.com>
2023-01-13 21:50:51 +0900

    3回目のコミットです

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    
+                                <a class="dropdown-item" href="{{route('user.show', ['id' =>  \Auth::user()->id])}}">
+                                    マイページ
+                                    </a>
+
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
